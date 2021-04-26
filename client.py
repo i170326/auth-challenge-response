2a33e3b469c7649ea350396da5cfcc29d6bd0dc5
i170326 <i170326@nu.edu.pk>
2021-04-26 09:26:54 +0500

    Test: Registeration

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import socket
 import sys
+import hashlib
+
 
 def tcp_connect(ip,port):
     sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,11 +17,47 @@ def tcp_receive(sock):
         full_msg = full_msg + msg.decode("utf-8")
     return full_msg
 
+def tcp_send(msg,conn):
+    print("Sending message")
+    conn.send(bytes(msg,"utf-8"))
+
+def tcp_close(conn):
+    print("Closing connection")
+    conn.close()
+
+def hashed_password():
+    u_password=(input("Enter password: "))
+    f_password=hashlib.sha512() #assignment requirement for hash algo
+    f_password.update(u_password.encode("utf-8"))
+    return f_password.hexdigest() #to return as string
+
+
 while True:
-    print("Connecting to server\n")
-    sock=tcp_connect('locahost',1234)
-    print("Receiving Message\n")
-    recvMsg = tcp_receive(sock)
-    if(len(recvMsg)>0):
-        print(recvMsg)
-        break
+    print("Connecting to server")
+    sock=tcp_connect('locahost',8001)
+    print("Connected\n")
+
+    #Menu - add UI later
+    option = (input("Choose from the following\n1. Press 1 for Registeration.\n2.Press 2 for Server's Message"))
+
+    if option=="1": #Registeration
+        tcp_send("1",sock) #Letting server know the option
+        recvMsg = tcp_receive(sock)
+        if(recvMsg=="Registerting\n"): #Server has started registering
+            username=input("Enter username: ")
+            tcp_send(username,sock) #Send username
+            tcp_send(hashed_password(),sock) #Send hashed password
+            recvMsg = tcp_receive(sock)
+            if(len(recvMsg)>0):
+                print(recvMsg)
+                tcp_close(sock)
+                break
+
+
+    elif option=="2": #Debugging connection
+        tcp_send("2",sock)
+        recvMsg = tcp_receive(sock)
+        if(len(recvMsg)>0):
+            print(recvMsg)
+            tcp_close(sock)
+            break
--- server.py
+++ server.py
@@ -14,6 +14,15 @@ def tcp_send(msg,conn):
     print("Sending message")
     conn.send(bytes(msg,"utf-8"))
 
+def tcp_receive(sock):
+    full_msg = ''
+    while True:
+        msg = sock.recv(8)
+        if len(msg) <=0:
+            break
+        full_msg = full_msg + msg.decode("utf-8")
+    return full_msg
+
 def tcp_close(conn):
     print("Closing connection")
     conn.close()
@@ -36,19 +45,40 @@ def db_table(conn):
     try:
         c = conn.cursor()
         c.execute(sql_create_table)
+        
     except Error as e:
         print(e)
+    
+def db_insert(conn,username,password):
+    try:
+        c = conn.cursor()
+        c.execute('insert into user (name, password) values (?, ?)', (username, password,))
+        conn.commit()
+    except Error as e:
+        print(e)    
 
 print("Connecting to db")
 db_conn=conn_db() #Connect to db
 print("Checking table")
 db_table(db_conn) #Create user table if it doesn't exist
 
-sock=tcp_connect('localhost',1234) #Setting up server
+sock=tcp_connect('localhost',8001) #Setting up server
 while True:
     print("Waiting for connection\n")
     connection, client_address = sock.accept()
     print("Connection with {address} has been established\n")
-    tcp_send("Hello this is server",connection)
-    tcp_close(connection)
+
+    recvMsg = tcp_receive(sock)
+    if(recvMsg=="1+\n"):
+        tcp_send("Registerting",sock) #Letting client know 
+        username=tcp_receive(sock) #Listen for username
+        password=tcp_receive(sock) #Listen for password
+        username=username.strip('\n') #Removing backslash
+        password=password.strip('\n')
+        db_insert(db_conn,username,password)
+        tcp_send("Registered",sock)
+
+    elif(recvMsg=="2+\n"): #Debugging Connection  
+        tcp_send("Hello this is server",connection)
+        tcp_close(connection)
 
